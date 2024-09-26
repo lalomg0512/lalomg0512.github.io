4ac4f20844862a4507b49f38c4d24d86cc59017e
lalomg0512 <jlrl01234@gmail.com>
2024-09-26 13:09:12 -0600

    update interests branch

--- src/app/interests/interests.component.ts
+++ src/app/interests/interests.component.ts
@@ -13,40 +13,7 @@ export class InterestsComponent implements OnInit {
 
   ngOnInit(): void {
     this.interests = [
-      {
-        categoria: "Tecnología",
-        items: [
-          "Inteligencia Artificial",
-          "Desarrollo de aplicaciones móviles",
-          "Internet de las cosas (IoT)"
-        ]
-      },
-      {
-        categoria: "Deportes",
-        items: [
-          "Fútbol",
-          "Natación",
-          "Ciclismo de montaña"
-        ]
-      },
-      {
-        categoria: "Arte y Cultura",
-        items: [
-          "Fotografía",
-          "Música clásica",
-          "Literatura contemporánea"
-        ]
-      },
-      {
-        categoria: "Viajes",
-        items: [
-          "Exploración de culturas diferentes",
-          "Senderismo en parques nacionales",
-          "Gastronomía internacional"
-        ]
-      }
+      // ... (el contenido que te proporcioné anteriormente)
     ];
-
-    console.log(this.interests);
   }
 }
